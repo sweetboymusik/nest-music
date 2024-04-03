bb60b1577270cc030508327da3ec0c63163bb82c
Elliott <ellbutt@gmail.com>
2024-04-03 10:15:13 -0230

    set up react router + nav and player updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import NavBar from "./components/NavBar/NavBar";
 import Player from "./components/Player/Player";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Player />
-    </div>
+    <Router>
+      <div className="app">
+        <NavBar />
+
+        <div className="content-container">
+          <Routes>
+            <Route
+              path="/home"
+              element={
+                <div>
+                  <h1>Title</h1>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                  <p>This is a test paragraph</p>
+                </div>
+              }
+            />
+            <Route path="/browse"></Route>
+            <Route paty="/library"></Route>
+          </Routes>
+        </div>
+
+        <Player />
+      </div>
+    </Router>
   );
 }
 
