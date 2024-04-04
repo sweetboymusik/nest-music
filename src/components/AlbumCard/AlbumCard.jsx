b457939abe751c4c33a94d3c33f6dce1f310ec42
Elliott <ellbutt@gmail.com>
2024-04-04 16:54:06 -0230

    album card work

--- src/components/AlbumCard/AlbumCard.jsx
+++ src/components/AlbumCard/AlbumCard.jsx
@@ -1,18 +1,17 @@
 import "./AlbumCard.css";
 
-fetch("nest-music/db.json")
-    .then(data => {
-        // Handle the JSON data
-        console.log(data);
-    });
+fetch("nest-music/db.json").then((data) => {
+  // Handle the JSON data
+  console.log(data);
+});
 
 function AlbumCard(album_name, url) {
-    return(
-        <div class='AlbumCard'> 
-            <img src={url} alt="Album" />
-            <h2> {album_name} </h2>
-        </div>
-    );
+  return (
+    <div class="AlbumCard">
+      <img src={url} alt="Album" />
+      <h2> {album_name} </h2>
+    </div>
+  );
 }
 
-export default AlbumCard
+export default AlbumCard;
---
+++ src/components/AlbumGrid/AlbumGrid.jsx
@@ -0,0 +1,7 @@
+import AlbumCard from "../AlbumCard/AlbumCard";
+
+function AlbumGrid() {
+  return <div>AlbumGrid</div>;
+}
+
+export default AlbumGrid;
