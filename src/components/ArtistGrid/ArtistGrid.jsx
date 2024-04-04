0959febd742f62a816720d5d6d80ff6034aa7123
jdflores19 <joseph.flores@keyin.com>
2024-04-04 11:48:15 -0500

    minor revision to Artist Grid

--- src/components/ArtistGrid/ArtistGrid.jsx
+++ src/components/ArtistGrid/ArtistGrid.jsx
@@ -1,18 +1,3 @@
-// import "./ArtistGrid.css";
-// import ArtistCard from "../ArtistCard/ArtistCard"; 
-
-// function ArtistGrid() {
-//     return (
-//       <div className="artist-grid">
-//             {/* Initial setup just to check if grid is working */}
-//             <ArtistCard artist="Artist 1" imageUrl="https://images.unsplash.com/photo-1574169208507-84376144848b" />
-//             <ArtistCard artist="Artist 2" imageUrl="https://images.unsplash.com/photo-1579547621113-e4bb2a19bdd6" />
-//             <ArtistCard artist="Artist 3" imageUrl="https://images.unsplash.com/photo-1541661538396-53ba2d051eed" />
-//       </div>
-//     );
-// }
-
-
 import React, { useEffect, useState } from 'react';
 import ArtistCard from "../ArtistCard/ArtistCard"; 
 import "./ArtistGrid.css";
@@ -21,7 +6,7 @@ function ArtistGrid() {
   const [artists, setArtists] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:4999/artistsData')
+    fetch('http://localhost:4999/artists')
       .then(response => response.json())
       .then(data => {
         const shuffled = data.sort(() => 0.5 - Math.random());
@@ -31,11 +16,11 @@ function ArtistGrid() {
 
   return (
     <div className="artist-grid">
-      {artists.map((artistData, index) => (
+      {artists.map((artists, index) => (
         <ArtistCard
           key={index}
-          artist={artistData.name}
-          imageUrl={artistData.photo}
+          artist={artists.name}
+          imageUrl={artists.photo}
         />
       ))}
     </div>
