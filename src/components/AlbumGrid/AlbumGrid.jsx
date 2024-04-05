90710c4e43322f91d75eb9f5e94259de99ff2d03
Elliott <ellbutt@gmail.com>
2024-04-05 11:56:30 -0230

    album card + album grid work (data passed to them successfully)

--- src/components/AlbumGrid/AlbumGrid.jsx
+++ src/components/AlbumGrid/AlbumGrid.jsx
@@ -1,7 +1,16 @@
+import "./AlbumGrid.css";
 import AlbumCard from "../AlbumCard/AlbumCard";
 
-function AlbumGrid() {
-  return <div>AlbumGrid</div>;
+function AlbumGrid({ artists }) {
+  return (
+    <div className="album-grid">
+      {artists.map((artist) =>
+        artist.releases.map((release) => (
+          <AlbumCard key={release.id} artist={artist} release={release} />
+        ))
+      )}
+    </div>
+  );
 }
 
 export default AlbumGrid;
