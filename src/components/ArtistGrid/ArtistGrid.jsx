9f8e732c088eed3998ad0dd24b3d0b4328fdf19a
ZachHulan155 <zachary.hulan@keyin.com>
2024-04-08 16:47:35 -0230

    Work on PageArtist

--- src/components/ArtistGrid/ArtistGrid.jsx
+++ src/components/ArtistGrid/ArtistGrid.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import ArtistCard from "../ArtistCard/ArtistCard";
 import "./ArtistGrid.css";
 
-function ArtistGrid({ artists }) {
+function ArtistGrid({ artists, GetArtist }) {
   return (
     <div className="artist-grid">
       {artists.map((artists, index) => (
@@ -10,6 +10,7 @@ function ArtistGrid({ artists }) {
           key={index}
           artist={artists.name}
           imageUrl={artists.photo}
+          GetArtist={GetArtist}
         />
       ))}
     </div>
--- src/components/PageArtist/PageArtist.jsx
+++ src/components/PageArtist/PageArtist.jsx
@@ -4,23 +4,22 @@ import { useState, useEffect } from 'react';
 import AlbumGrid from "../AlbumGrid/AlbumGrid";
 
 
-function PageArtist({artists, name}) {
-  let [artist, setArtist] = useState()
-
-  function GetArtist() {
-    let result = artists.filter((artist) => 
-      artist.name === name 
-    )
-    setArtist(result) 
-    console.log(artist)
-  }
-  useEffect(() => {GetArtist()}, [])
-
+function PageArtist({artist}) {
+    // gets artist for PageArtist
+    let [artist, setArtist] = useState()
+    function GetArtist(name) {
+      let result = artists.filter((artist) => 
+        artist.name === name 
+      )
+      setArtist(result) 
+      console.log(artist)
+    }
+    useState(() => {GetArtist("Amara")}, [artists])
+    useState(() => {console.log("hello")}, [artist])
   return (
     <div>
       
       <AlbumGrid artists={artist}/>
-
     </div>
   )
 }
