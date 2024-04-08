9f899aeb7e208acaaebe1b44943109a030cd9796
jdflores19 <joseph.flores@keyin.com>
2024-04-08 12:22:04 -0500

    change source of release image

--- src/components/SongCard/SongCard.jsx
+++ src/components/SongCard/SongCard.jsx
@@ -12,7 +12,7 @@ function SongCard({ artist, release, track }) {
   return (
     <div className="song-card">
       <button className="song-play">
-        <img src={release.artwork} alt="album artwork" className="song-image" />
+        <img src={require(`../../assets/images/artwork/${release.artwork}`)} alt="album artwork" className="song-image" />
       </button>
 
       <div className="song-info">
@@ -60,34 +60,3 @@ function SongCard({ artist, release, track }) {
 }
 
 export default SongCard;
-
-// import { useState } from "react";
-// import { FaPlus, FaHeart, FaRegHeart } from "react-icons/fa6";
-// import tempImage from "../../assets/images/album_placeholder.png";
-
-// function SongCard() {
-//   let [hovered, setHoverd] = useState(false);
-
-//   return (
-//     <div className="song-card">
-//       <img src={tempImage} alt="" className="song-image" />
-//       <div className="song-info">
-//         <span className="song-text">
-//           <b>Title</b>
-//         </span>
-//         <span className="song-text">Artist</span>
-//         <span className="song-text">Album - YEAR</span>
-//       </div>
-//       <div className="song-icons">
-//         <button className="song-heart-btn">
-//           <FaRegHeart className="song-icon" />
-//         </button>
-//         <button>
-//           <FaPlus className="song-icon" />
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default SongCard;
