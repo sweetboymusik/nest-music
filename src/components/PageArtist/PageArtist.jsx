f619aacca966261f874f58a468c24cdd369d55f1
ZachHulan155 <zachary.hulan@keyin.com>
2024-04-09 13:34:54 -0230

    Added header to PageArtist

--- src/components/PageArtist/PageArtist.jsx
+++ src/components/PageArtist/PageArtist.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import { useState, useEffect } from "react";
 import AlbumGrid from "../AlbumGrid/AlbumGrid";
 
+import "./PageArtist.css"
+
 function PageArtist({ currentArtist, getArtist }) {
   return (
-    <div>
-      <AlbumGrid artists={currentArtist} getArtist={getArtist} />
+    <div className="artist-main">
+      <div className="artist-header">
+        {currentArtist.map((artist) => 
+
+          <img className="artist-header-img"
+           src={require(`../../assets/images/artists/${artist.photo}`)}
+           alt="artist image"/>
+        )}
+        {currentArtist.map((artist) => 
+          <div className="artist-header-text">{artist.name}</div>
+        )}
+        
+      </div> 
+    
+      <div className="artist-body">
+        <AlbumGrid artists={currentArtist} getArtist={getArtist} />
+      </div>
     </div>
   );
 }
